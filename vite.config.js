518cdc836febebe683866fec1d4ec4d1f70f2e1f
Rochak <rochakbhandari7@gmail.com>
2025-06-25 21:32:12 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // listen on all network interfaces
+    port: 3000,        // optional: your preferred port
+    strictPort: true,  // fail if port is busy
+  }
 })
